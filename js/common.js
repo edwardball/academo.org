eb966c37522c1d1bb414c7ac71eb278ed5431b20
Edward Ball <info@aptary.com>
2016-04-05 19:54:19 +0100

    Add new social media buttons, including Google Classroom

--- js/common.js
+++ js/common.js
@@ -125,22 +125,3 @@ $(document).ready(function(){
 	}
 
 });
-
-
-
-// $(window).on("load", function() {
-// 	if ($("#demo").length){
-// 		faceyb(document, 'script', 'facebook-jssdk');
-// 		twitter(document, 'script', 'twitter-wjs');
-// 	}
-// });
-
-function faceyb(d, s, id) {
-  var js, fjs = d.getElementsByTagName(s)[0];
-  if (d.getElementById(id)) return;
-  js = d.createElement(s); js.id = id;
-  js.src = "//connect.facebook.net/en_GB/sdk.js#xfbml=1&version=v2.0";
-  fjs.parentNode.insertBefore(js, fjs);
-};
-
-function twitter(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+'://platform.twitter.com/widgets.js';fjs.parentNode.insertBefore(js,fjs);}}
