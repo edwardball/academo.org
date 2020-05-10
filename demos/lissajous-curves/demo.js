d5c63724c0383bf36c6caa951989f790a6851400
edwardballqgitqq <edwardjball@gmail.com>
2020-05-10 14:15:22 +0100

    Change coordinate system on Lissajous demo so increase in y points upwards

--- demos/lissajous-curves/demo.js
+++ demos/lissajous-curves/demo.js
@@ -84,7 +84,7 @@ var demo = new Demo({
 		// for (var i = angleStep ; i <= period ; i+=angleStep){
 		for (var i = 1 ; i <= numberOfPoints ; i++){
 			var nextPoint = this.curve(m,n, this.ui.radiusX.value, this.ui.radiusY.value, i*angleStep);
-			pathData += 'L' + nextPoint.x + "," + nextPoint.y + " ";
+			pathData += 'L' + nextPoint.x + "," + nextPoint.y*-1 + " "; // multiply y coordinate by -1 to make sure increase in y goes up on chart instead of down.
 		}
 		$(".js-curve-path").attr("d", pathData);
 		$(".js-curve-path").attr("transform", 'translate('+this.width/2+' '+this.height/2+')');
