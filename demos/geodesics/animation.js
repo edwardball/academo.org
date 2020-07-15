12e1f07014b235d0eb00eb796046f75e839e9601
edwardballqgitqq <edwardjball@gmail.com>
2020-07-15 12:27:42 +0100

    Fix broken geodesics demo

--- demos/geodesics/animation.js
+++ demos/geodesics/animation.js
@@ -16,58 +16,7 @@
     },
   };
 
-  //google earth
-  var ge;
-  var lineString;
-
-  function init() {
-    google.earth.createInstance('map3d', initCB, failureCB);
-    // initialize();
-
-  }
-
-  function failureCB(errorCode) {
-  }
-
-  function initCB(instance) {
-        ge = instance;
-        ge.getWindow().setVisibility(true);
-        ge.getNavigationControl().setVisibility(ge.VISIBILITY_SHOW);
-
-
-        // Create the placemark
-        var lineStringPlacemark = ge.createPlacemark('');
-
-        // Create the LineString
-        lineString = ge.createLineString('');
-        lineStringPlacemark.setGeometry(lineString);
-        lineString.setExtrude(true);
-
-        // Add LineString points
-        var startLat = 39.64;
-        var startLng = -115.49;
-
-        lineString.getCoordinates().pushLatLngAlt(39.64, -115.49, 0);
-        for (i = 0, pointCount = 20 ; i < pointCount ; i++){
-          middle = google.maps.geometry.spherical.interpolate(new google.maps.LatLng(39.64, -115.49), new google.maps.LatLng(-22.69, 52.98), i / pointCount);
-          lineString.getCoordinates().pushLatLngAlt(middle.lat(), middle.lng(), 0);
-        }
-        lineString.getCoordinates().pushLatLngAlt(-22.69, 52.98, 0);
-
-
-        //style it
-        // Create a style and set width and color of line
-        lineStringPlacemark.setStyleSelector(ge.createStyle(''));
-        var lineStyle = lineStringPlacemark.getStyleSelector().getLineStyle();
-        lineStyle.setWidth(5);
-        lineStyle.getColor().set('99CC0099');  // aabbggrr format
-
-        // Add the feature to Earth
-        ge.getFeatures().appendChild(lineStringPlacemark);
-
-        initialize();
-
-      }
+ 
 
   $(document).on("uiLoaded", function(){
 
@@ -81,12 +30,7 @@
     map_dom = document.getElementById('demo');
     map_dom.style.height = map_dom.offsetWidth/2  + "px";
 
-
-    google.load("earth", "1", {"other_params":"sensor=false"});
-
-    // //calbacks
-
-    google.setOnLoadCallback(initialize);
+    initialize();
 
 });
 
@@ -98,10 +42,6 @@
         mapTypeId: google.maps.MapTypeId.ROADMAP
       };
       var map = new google.maps.Map(map_dom, myOptions);
-
-      if (typeof GoogleEarth != 'undefined') {
-        var ge = new GoogleEarth(map);
-      }
 
 
       marker1 = new google.maps.Marker({
